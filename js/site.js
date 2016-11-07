c1ecfc8f41ae888ccd43cda057b8ee2cd50a1d85
scottflorisson <gh@webngen.net>
2016-11-08 07:14:20 +0800

    Add missing files from last commit

---
+++ js/site.js
@@ -0,0 +1,34 @@
+/// <reference path="../typings/globals/angular/index.d.ts" />
+var mod;
+var MySite;
+(function (MySite) {
+    var Bootstrapper = (function () {
+        function Bootstrapper() {
+        }
+        Bootstrapper.prototype.init = function () {
+            var mod = angular.module("mysite", ['ui.router']);
+            mod.controller("DashboardController", function () {
+            });
+            mod.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", '$httpProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+                    $stateProvider
+                        .state('dashboard', {
+                        url: "/",
+                        templateUrl: "/partials/home/index"
+                    });
+                    $urlRouterProvider.otherwise('/');
+                    $locationProvider.html5Mode(true);
+                }]);
+            mod.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+                    $rootScope.$state = $state;
+                    $rootScope.$on('$locationChangeStart', function (event, toUrl, fromUrl) {
+                        console.log(toUrl);
+                    });
+                }]);
+            angular.bootstrap(document.getElementsByTagName("html")[0], ['mysite']);
+        };
+        return Bootstrapper;
+    }());
+    MySite.Bootstrapper = Bootstrapper;
+})(MySite || (MySite = {}));
+var bs = new MySite.Bootstrapper();
+bs.init();
---
+++ js/site.ts
@@ -0,0 +1,46 @@
+/// <reference path="../typings/globals/angular/index.d.ts" />
+var mod: ng.IModule;
+
+namespace MySite {
+
+    export class Bootstrapper {
+
+        public init() {
+            var mod = angular.module("mysite", ['ui.router']);
+
+            mod.controller("DashboardController",
+            () => {
+
+            });
+
+            mod.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", '$httpProvider', ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) => {
+                $stateProvider
+                    .state('dashboard',
+                    {
+                        url: "/",
+                        templateUrl: "/partials/home/index"
+                    })
+                    ;
+
+                $urlRouterProvider.otherwise('/');
+                $locationProvider.html5Mode(true);
+            }]);
+
+
+            mod.run(['$rootScope', '$state', '$stateParams', ($rootScope, $state, $stateParams) => {
+                (<any>$rootScope).$state = $state;
+                $rootScope.$on('$locationChangeStart', (event, toUrl, fromUrl) => {
+                    console.log(toUrl);
+                });
+            }]);
+
+
+
+            angular.bootstrap(document.getElementsByTagName("html")[0], ['mysite']);
+
+        }
+    }
+}
+
+var bs = new MySite.Bootstrapper();
+bs.init();
